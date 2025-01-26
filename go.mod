3dd45b8b58877f07f75d7265f25b2eaf7bd8ef7e
kettek <kts@kettek.net>
2025-01-26 01:04:52 -0800

    Add server list generation from metaserver

--- go.mod
+++ go.mod
@@ -1,6 +1,8 @@
 module github.com/kettek/mobifire
 
-go 1.23.0
+go 1.23.2
+
+toolchain go1.23.5
 
 require fyne.io/fyne/v2 v2.5.3
 
@@ -13,6 +15,8 @@ require (
 	github.com/fyne-io/gl-js v0.0.0-20220119005834-d2da28d9ccfe // indirect
 	github.com/fyne-io/glfw-js v0.0.0-20241126112943-313d8a0fe1d0 // indirect
 	github.com/fyne-io/image v0.0.0-20220602074514-4956b0afb3d2 // indirect
+	github.com/gdamore/encoding v1.0.1 // indirect
+	github.com/gdamore/tcell/v2 v2.7.4 // indirect
 	github.com/go-gl/gl v0.0.0-20211210172815-726fda9656d6 // indirect
 	github.com/go-gl/glfw/v3.3/glfw v0.0.0-20240506104042-037f3cc74f2a // indirect
 	github.com/go-text/render v0.2.0 // indirect
@@ -21,8 +25,15 @@ require (
 	github.com/gopherjs/gopherjs v1.17.2 // indirect
 	github.com/jeandeaual/go-locale v0.0.0-20240223122105-ce5225dcaa49 // indirect
 	github.com/jsummers/gobmp v0.0.0-20151104160322-e2ba15ffa76e // indirect
+	github.com/kettek/go-multipath/v2 v2.0.0-alpha.11 // indirect
+	github.com/kettek/termfire v0.0.0-20250126003125-1910273f5881 // indirect
+	github.com/lucasb-eyer/go-colorful v1.2.0 // indirect
+	github.com/mattn/go-runewidth v0.0.16 // indirect
 	github.com/nicksnyder/go-i18n/v2 v2.4.0 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/qdm12/reprint v0.0.0-20200326205758-722754a53494 // indirect
+	github.com/rivo/tview v0.0.0-20241103174730-c76f7879f592 // indirect
+	github.com/rivo/uniseg v0.4.7 // indirect
 	github.com/rymdport/portal v0.3.0 // indirect
 	github.com/srwiley/oksvg v0.0.0-20221011165216-be6e8873101c // indirect
 	github.com/srwiley/rasterx v0.0.0-20220730225603-2ab79fcdd4ef // indirect
@@ -31,7 +42,8 @@ require (
 	golang.org/x/image v0.18.0 // indirect
 	golang.org/x/mobile v0.0.0-20231127183840-76ac6878050a // indirect
 	golang.org/x/net v0.25.0 // indirect
-	golang.org/x/sys v0.20.0 // indirect
-	golang.org/x/text v0.16.0 // indirect
+	golang.org/x/sys v0.27.0 // indirect
+	golang.org/x/term v0.26.0 // indirect
+	golang.org/x/text v0.20.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- states/metaserver/state.go
+++ states/metaserver/state.go
@@ -1,16 +1,29 @@
 package metaserver
 
 import (
+	"io"
+	"net/http"
+	"time"
+
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/container"
 	"fyne.io/fyne/v2/layout"
 	"fyne.io/fyne/v2/widget"
 	"github.com/kettek/mobifire/states"
 	"github.com/kettek/mobifire/states/play"
+	"github.com/kettek/termfire/debug"
+	"github.com/kettek/termfire/messages"
 )
 
+var metaservers = []string{
+	"http://crossfire.real-time.com/metaserver2/meta_client.php",
+	"http://metaserver.eu.cross-fire.org/meta_client.php",
+	"http://metaserver.us.cross-fire.org/meta_client.php",
+}
+
 type State struct {
-	container *fyne.Container
+	container  *fyne.Container
+	serverList *fyne.Container
 }
 
 func (s *State) Enter(next func(states.State)) (leave func()) {
@@ -18,11 +31,70 @@ func (s *State) Enter(next func(states.State)) (leave func()) {
 		next(&play.State{})
 	})
 
-	s.container = container.New(layout.NewCenterLayout(), button)
+	s.serverList = container.New(layout.NewVBoxLayout())
+
+	s.container = container.New(layout.NewVBoxLayout(), s.serverList, button)
+
+	// Load servers on load, obv.
+	s.refreshMetaservers()
 
 	return nil
 }
 
+func (s *State) refreshMetaservers() {
+	s.serverList.RemoveAll()
+	var serverEntries messages.ServerEntries
+	for _, m := range metaservers {
+		entries, err := s.requestServers(m)
+		if err != nil {
+			debug.Debug("Failed to get server list from metaserver: ", err)
+			continue
+		}
+		for _, e := range entries {
+			found := false
+			for _, server := range serverEntries {
+				if server.Hostname == e.Hostname && server.Port == e.Port {
+					found = true
+					break
+				}
+			}
+			if !found {
+				serverEntries = append(serverEntries, e)
+			}
+		}
+	}
+
+	accordion := widget.NewAccordion()
+	for _, e := range serverEntries {
+		acc := widget.NewAccordionItem(e.Hostname, widget.NewLabel(e.TextComment))
+		accordion.Append(acc)
+	}
+	s.serverList.Add(accordion)
+}
+
 func (s *State) Container() *fyne.Container {
 	return s.container
+}
+
+func (s *State) requestServers(metaserver string) (messages.ServerEntries, error) {
+	resp, err := http.Get(metaserver)
+	http.DefaultClient.Timeout = 5 * time.Second
+	if err != nil {
+		return nil, err
+	}
+
+	defer resp.Body.Close()
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	serverEntries := messages.ServerEntries{}
+
+	err = serverEntries.UnmarshalBinary(body)
+	if err != nil {
+		return nil, err
+	}
+
+	return serverEntries, nil
 }
