2d58678b6cdf70ea351bab4396c6dd0487491223
kettek <kts@kettek.net>
2025-01-28 15:59:26 -0800

    Minor cleanup

--- cmd/mobifire/main.go
+++ cmd/mobifire/main.go
@@ -1,16 +1,5 @@
 package main
 
-import (
-	_ "embed"
-)
-
 func main() {
 	NewGame()
-	/*mb := NewMultiBoard(11, 11, 8)
-
-	center := container.New(layout.NewCenterLayout(), mb.container)
-
-	w.SetContent(center)
-
-	w.ShowAndRun()*/
 }
