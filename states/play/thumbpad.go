460b9c07ce01129c2fc439b05d2dcbc776a3916c
kettek <kts@kettek.net>
2025-01-29 04:19:56 -0800

    Add thumbpad wip

---
+++ states/play/thumbpad.go
@@ -0,0 +1,72 @@
+package play
+
+import (
+	"image/color"
+	"log"
+
+	"fyne.io/fyne/v2"
+	"fyne.io/fyne/v2/canvas"
+	"fyne.io/fyne/v2/theme"
+	"fyne.io/fyne/v2/widget"
+)
+
+type thumbpadWidget struct {
+	widget.BaseWidget
+}
+
+func (r *thumbpadWidget) CreateRenderer() fyne.WidgetRenderer {
+	r.ExtendBaseWidget(r)
+	return &thumbpadWidgetRenderer{
+		rect: canvas.NewRectangle(theme.PrimaryColor()),
+	}
+}
+
+func (r *thumbpadWidget) MinSize() fyne.Size {
+	r.ExtendBaseWidget(r)
+	return r.BaseWidget.MinSize()
+}
+
+func (r *thumbpadWidget) Tapped(event *fyne.PointEvent) {
+	log.Println("Tapped", event)
+}
+
+func (r *thumbpadWidget) TappedSecondary(event *fyne.PointEvent) {
+	log.Println("TappedSecondary", event)
+}
+
+func (r *thumbpadWidget) Dragged(event *fyne.DragEvent) {
+	log.Println("Dragged", event)
+}
+
+func (r *thumbpadWidget) DragEnd() {
+	log.Println("DragEnd")
+}
+
+var _ fyne.WidgetRenderer = (*thumbpadWidgetRenderer)(nil)
+
+type thumbpadWidgetRenderer struct {
+	rect *canvas.Rectangle
+}
+
+func (r *thumbpadWidgetRenderer) BackgroundColor() color.Color {
+	return color.Transparent
+}
+
+func (r *thumbpadWidgetRenderer) Destroy() {
+}
+
+func (r *thumbpadWidgetRenderer) Layout(size fyne.Size) {
+	r.rect.Resize(size)
+}
+
+func (r *thumbpadWidgetRenderer) MinSize() fyne.Size {
+	return r.rect.MinSize()
+}
+
+func (r *thumbpadWidgetRenderer) Objects() []fyne.CanvasObject {
+	return []fyne.CanvasObject{r.rect}
+}
+
+func (r *thumbpadWidgetRenderer) Refresh() {
+	r.rect.Refresh()
+}
