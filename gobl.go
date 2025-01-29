b5977d5e0cacde5b1baef56d83146148da135caa
kettek <kts@kettek.net>
2025-01-28 23:30:10 -0800

    Add android build, install, and watch/run tasks
    Also a crummy icon

--- gobl.go
+++ gobl.go
@@ -26,5 +26,24 @@ func main() {
 		Run("build").
 		Run("run")
 
+	Task("build-mobile").
+		Chdir("cmd/mobifire").
+		Exec("fyne", "package", "-os", "android", "-appID", "net.kettek.mobifire", "-icon", "icon.png")
+
+	Task("run-mobile").
+		Exec("adb", "shell", "am", "force-stop", "net.kettek.mobifire").
+		Exec("adb", "shell", "am", "start", "-a", "android.intent.action.MAIN", "-n", "net.kettek.mobifire/org.golang.app.GoNativeActivity")
+
+	Task("install-mobile").
+		Chdir("cmd/mobifire").
+		Exec("adb", "install", "./mobifire.apk")
+
+	Task("watch-mobile").
+		Watch("states/**/*.go", "net/**/*.go").
+		Run("build-mobile").
+		Run("install-mobile").
+		Run("run-mobile").
+		Sleep("1y")
+
 	Go()
 }
