d52802bb54bd08c26aa3dca85060d5dd8ce6a55a
kettek <kts@kettek.net>
2025-02-18 18:15:40 -0800

    Implement action save/load and triggering
    No spells yet, since need to implement that side of things...

--- states/play/managers/action/entry.go
+++ states/play/managers/action/entry.go
@@ -1,30 +1,167 @@
 package action
 
 import (
+	"encoding/base64"
+	"encoding/json"
+	"fmt"
+
 	"fyne.io/fyne/v2"
+	"github.com/kettek/mobifire/data"
 	"github.com/kettek/termfire/messages"
 )
 
-type Kind int
-
-const (
-	KindNone Kind = iota
-	KindApplyFromInventory
-	KindReadySkill
-	KindUseSkill
-	KindReadySpell
-	KindCastSpell
-)
-
-type Entry struct {
-	Kind         Kind
-	Image        fyne.Resource
-	InventoryTag int32  // The inventory to target. This will generally always be the player.
-	ObjectTag    int32  // The ID of the object. This is cached and used when possible. If the ObjectTag does not exist, the Name will be used to do a lookup.
-	Name         string // Item name, skill name, or spell name
-	OnTrigger    func(messages.Message)
+type EntryApplyKind struct {
+	ObjectName      string // The name of the object. This is used to do a lookup.
+	objectTag       int32  // The ID of the object. This is cached and used when possible. If the ObjectTag does not exist, the Name will be used to do a lookup.
+	OnlyIfUnapplied bool
+	exists          bool // This is set to true once the action has been triggered and the object tag is known to exist.
 }
 
-func (e Entry) Trigger() {
-	// TODO: ???
+type EntrySkillKind struct {
+	Skill int32
+	Name  string
+	Ready bool // Whether to ready or use the skill
+}
+
+type EntrySpellKind struct {
+	Spell int32
+	Name  string
+	Ready bool // Whether to ready or cast the spell
+}
+
+type EntryCommandKind struct {
+	Command string
+	Repeat  int
+}
+
+type Entry struct {
+	Image fyne.Resource
+	Kind  interface{}
+}
+
+func NewEntryFromString(data string) *Entry {
+	var entry Entry
+	json.Unmarshal([]byte(data), &entry)
+	return &entry
+}
+
+type kindWrapper struct {
+	Kind  string
+	Value json.RawMessage
+}
+
+type entryWrapper struct {
+	Image string // base64 data of image
+	Data  kindWrapper
+}
+
+func (e *Entry) MarshalJSON() ([]byte, error) {
+	var kind string
+	var value []byte
+
+	var err error
+	switch e.Kind.(type) {
+	case EntryApplyKind:
+		kind = "apply"
+		value, err = json.Marshal(e.Kind.(EntryApplyKind))
+	case EntrySkillKind:
+		kind = "skill"
+		value, err = json.Marshal(e.Kind.(EntrySkillKind))
+	case EntrySpellKind:
+		kind = "spell"
+		value, err = json.Marshal(e.Kind.(EntrySpellKind))
+	case EntryCommandKind:
+		kind = "command"
+		value, err = json.Marshal(e.Kind.(EntryCommandKind))
+	}
+	if err != nil {
+		return nil, err
+	}
+
+	dst := make([]byte, base64.StdEncoding.EncodedLen(len(e.Image.Content())))
+	base64.StdEncoding.Encode(dst, e.Image.Content())
+
+	wrapper := entryWrapper{
+		Image: string(dst),
+		Data: kindWrapper{
+			Kind:  kind,
+			Value: json.RawMessage(value),
+		},
+	}
+
+	return json.Marshal(wrapper)
+}
+
+func (e *Entry) Unmarshal(index int, b []byte) error {
+	var wrapper entryWrapper
+	if err := json.Unmarshal(b, &wrapper); err != nil {
+		// TODO: Adjust entry to show it is erroneous.
+		return nil
+	}
+
+	if wrapper.Data.Kind == "apply" {
+		var kind EntryApplyKind
+		if err := json.Unmarshal(wrapper.Data.Value, &kind); err != nil {
+			return err
+		}
+		e.Kind = kind
+	} else if wrapper.Data.Kind == "skill" {
+		var kind EntrySkillKind
+		if err := json.Unmarshal(wrapper.Data.Value, &kind); err != nil {
+			return err
+		}
+		e.Kind = kind
+	} else if wrapper.Data.Kind == "spell" {
+		var kind EntrySpellKind
+		if err := json.Unmarshal(wrapper.Data.Value, &kind); err != nil {
+			return err
+		}
+		e.Kind = kind
+	} else if wrapper.Data.Kind == "command" {
+		var kind EntryCommandKind
+		if err := json.Unmarshal(wrapper.Data.Value, &kind); err != nil {
+			return err
+		}
+		e.Kind = kind
+	}
+
+	b64, err := base64.StdEncoding.DecodeString(wrapper.Image)
+	if err != nil {
+		fmt.Println("Error decoding image:", err)
+		e.Image = data.GetResource("blank.png")
+		return nil // Don't return error, as we shouldn't explode if something wack happened to the embedded image.
+	}
+	e.Image = fyne.NewStaticResource(fmt.Sprintf("entry_%d", index), b64)
+
+	return nil
+}
+
+func (e Entry) Trigger(m *Manager) {
+	switch k := e.Kind.(type) {
+	case EntryApplyKind:
+		if !k.exists {
+			// Lookup the given object by name... I guess search _all_ inventories for now.
+			if item := m.itemsManager.GetItemByName(k.ObjectName); item != nil {
+				k.objectTag = item.Tag
+				k.exists = true
+			}
+		}
+		if item := m.itemsManager.GetItemByTag(k.objectTag); item != nil {
+			if k.OnlyIfUnapplied && item.Flags.Applied() {
+				return
+			}
+			m.conn.Send(&messages.MessageApply{
+				Tag: k.objectTag,
+			})
+		}
+	case EntrySkillKind:
+		// TODO: Maybe add ready and use skill option? This would ensure that a talisman or holy symbol gets equipped before using the skill.
+		if k.Ready {
+			m.conn.SendCommand("ready_skill "+k.Name, 1)
+		} else {
+			m.conn.SendCommand("use_skill "+k.Name, 1)
+		}
+	case EntryCommandKind:
+		m.conn.SendCommand(k.Command, uint32(k.Repeat))
+	}
 }
--- states/play/managers/action/manager.go
+++ states/play/managers/action/manager.go
@@ -1,6 +1,10 @@
 package action
 
 import (
+	"fmt"
+
+	"fyne.io/fyne/v2"
+	"github.com/kettek/mobifire/net"
 	"github.com/kettek/mobifire/states/play/managers"
 	"github.com/kettek/mobifire/states/play/managers/items"
 	"github.com/kettek/mobifire/states/play/managers/skills"
@@ -8,6 +12,8 @@ import (
 
 // Manager provides management of running and setting actions. Actions are tied to buttons that do some sort of dynamic action that is determined by the player, such as equipping an item, drinking a potion, readying a skill, etc.
 type Manager struct {
+	app           fyne.App
+	conn          *net.Connection
 	entries       []Entry
 	skillsManager *skills.Manager
 	itemsManager  *items.Manager
@@ -18,7 +24,49 @@ func NewManager() *Manager {
 }
 
 func (m *Manager) Init() {
-	// TODO: Load actions from files?
+	m.loadActions()
+}
+
+func (m *Manager) loadActions() error {
+	m.entries = nil
+	entryStrings := m.app.Preferences().StringList("actions")
+	for i, entryString := range entryStrings {
+		var entry Entry
+		err := entry.Unmarshal(i, []byte(entryString))
+		if err != nil {
+			fmt.Println("Error unmarshalling entry:", err)
+			continue
+		}
+		m.entries = append(m.entries, entry)
+	}
+	return nil
+}
+
+func (m *Manager) saveActions() error {
+	entryStrings := make([]string, len(m.entries))
+	for i, entry := range m.entries {
+		b, err := entry.MarshalJSON()
+		if err != nil {
+			fmt.Println("Error marshalling entry:", err)
+			continue
+		}
+		entryStrings[i] = string(b)
+	}
+	m.app.Preferences().SetStringList("actions", entryStrings)
+	return nil
+}
+
+func (m *Manager) SetApp(app fyne.App) {
+	m.app = app
+}
+
+func (m *Manager) SetAction(i int, entry Entry) {
+	// Grow as needed.
+	if i >= len(m.entries) {
+		m.entries = append(m.entries, make([]Entry, i-len(m.entries)+1)...)
+	}
+	m.entries[i] = entry
+	m.saveActions()
 }
 
 func (m *Manager) Action(i int) *Entry {
@@ -26,6 +74,14 @@ func (m *Manager) Action(i int) *Entry {
 		return nil
 	}
 	return &m.entries[i]
+}
+
+func (m *Manager) TriggerAction(i int) {
+	if i < 0 || i >= len(m.entries) {
+		return
+	}
+	entry := m.entries[i]
+	entry.Trigger(m)
 }
 
 func (m *Manager) SetManagers(managers *managers.Managers) {
@@ -37,4 +93,8 @@ func (m *Manager) SetManagers(managers *managers.Managers) {
 			m.itemsManager = manager
 		}
 	}
+}
+
+func (m *Manager) SetConnection(conn *net.Connection) {
+	m.conn = conn
 }
